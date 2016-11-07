047c547e7457193846b7d2407225ce9f4033be68
Javier Amor Garcia <jamor@qdqmedia.com>
2016-11-07 14:54:11 +0100

    Up version to 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="QDQ media",
     author_email="dpto-tecnico@qdqmedia.com",
     name='django-actions',
-    version='0.0.1',
+    version='0.0.2',
     description='',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     url='https://github.com/qdqmedia/django-actions/',
